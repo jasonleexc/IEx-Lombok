c18f10897fe57a4cd9c8790aa661b02d4ef0d41d
jasonleexc <e1398491@u.nus.edu>
2025-10-26 15:43:08 +0800

    Add Registration Functionality in frontend (without storing in database)

--- backend/models/sighting.py
+++ backend/models/sighting.py
@@ -17,12 +17,12 @@ class Sighting(db.Model):
     sighting_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    """ TODO: work on image recognition in later versions
+    # TODO: work on image recognition in later versions
     # Computer vision analysis results
-    ai_species_prediction = db.Column(db.String(50), nullable=True)
-    ai_confidence = db.Column(db.Float, nullable=True)
-    analysis_metadata = db.Column(db.JSON, nullable=True)  # Store additional AI analysis data
-    """"
+    # ai_species_prediction = db.Column(db.String(50), nullable=True)
+    # ai_confidence = db.Column(db.Float, nullable=True)
+    # analysis_metadata = db.Column(db.JSON, nullable=True)  # Store additional AI analysis data
+
     def __repr__(self):
         return f"Sighting('{self.species}', '{self.location}', '{self.sighting_date}')"
 
@@ -41,9 +41,9 @@ class Sighting(db.Model):
             'sighting_date': self.sighting_date.isoformat(),
             'user_id': self.user_id,
             'reporter': self.reporter.username if self.reporter else None,
-            """ TODO: add in later on once image recognition software is up
-            'ai_species_prediction': self.ai_species_prediction,
-            'ai_confidence': self.ai_confidence,
-            'analysis_metadata': self.analysis_metadata
-            """
+            
+            # TODO: add in later on once image recognition software is up
+            # 'ai_species_prediction': self.ai_species_prediction,
+            # 'ai_confidence': self.ai_confidence,
+            # 'analysis_metadata': self.analysis_metadata
         }
